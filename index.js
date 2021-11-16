bcb213f13c6cb4e6a537db6adc5c7696add8a88c
ndumanska <94489750+ndumanska@users.noreply.github.com>
2021-11-16 23:34:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function showMoreFunc() {
+    let container = document.getElementById("overflow");
+    let button = document.getElementById("btn");
+    container.classList.toggle("show");
+    button.classList.toggle("opened");
+  }
